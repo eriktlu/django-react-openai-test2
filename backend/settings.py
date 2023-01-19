a9d1eb85f19772de5b37cb147110ec8e5a08ecef
eriktlu <36303097+eriktlu@users.noreply.github.com>
2023-01-19 23:14:59 +0800

    Test

--- backend/settings.py
+++ backend/settings.py
@@ -113,12 +113,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/')
-STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'frontend\\static'),
-# )
+STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_URL = 'static/'
 
 # print(STATIC_URL)
 # print(STATIC_ROOT)
